9d3ca56d1cf6a256aad9eade4f2ff3d43486742c
Hamid Reza Arzbin <152203026+hamidreza-arzbin@users.noreply.github.com>
2026-07-02 17:19:50 +0330

    add css to login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,11 +7,12 @@
 
     <title>ورود به سامانه نظرسنجی</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
-          rel="stylesheet">
+    <link href="{{ asset('admin-assets/css/styles.css') }}" rel="stylesheet">
+    <link href="{{ asset('admin-assets/css/bootstrap.rtl.min.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('admin-assets/css/bootstrap-icons.min.css') }}">
 
-    <link rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <script src="{{ asset('admin-assets/js/jquery-4.0.0.min.js') }}"></script>
+    <script src="{{ asset('admin-assets/js/bootstrap.bundle.min.js') }}"></script>
 
     <style>
         body {
